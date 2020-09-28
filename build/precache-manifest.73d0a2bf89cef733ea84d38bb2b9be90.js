2184f411a5fdb9bbdc1ed21daae51c5f1ec92708
Sandro444 <tsereteli.sandro0401@gmail.com>
2020-09-28 14:50:59 +0400

    add static files
    add react  "run buid" files

---
+++ build/precache-manifest.73d0a2bf89cef733ea84d38bb2b9be90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9ce14fcc353b2a1ebfa60317bee7141",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d3fd27aafa15d0b013f",
+    "url": "/static/css/main.3c266156.chunk.css"
+  },
+  {
+    "revision": "6c0f444ebba93378dbff",
+    "url": "/static/js/2.19ed25a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.19ed25a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d3fd27aafa15d0b013f",
+    "url": "/static/js/main.8b2f5102.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const cors = require('cors')
 
 app.use(express.json());
 app.use(cors())
+app.use(express.static('build'))
+
 
 morgan.token('number', req=>{
     return req.body.number
